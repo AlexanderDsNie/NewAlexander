a55e001f63ff4d1db21f14934121800e1b720dfa
AlexanderDsNie <997951737@qq.com>
2019-09-21 11:26:32 +0800

    kop

--- test2/src/App.vue
+++ test2/src/App.vue
@@ -1,29 +1,33 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+      <common-header></common-header>
+      <div class="container">
+          fyghjkl;'lhaha
+        header'
+        <h2>hehe</h2>
+      </div>
+      <common-footer></common-footer>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+  //  1 导入  
+  //  2 components中声明 
+  //  3template中 使用
+    import CommonHeader from './components/CommonHeader';
+    // import HelloWorld from './components/HelloWorld';
+    import CommonFooter from './components/CommonFooter';
+    export default {
+        components:{
+            CommonHeader,
+            CommonFooter
+            // HelloWorld
+        }
     }
+</script>
+
+<style lang="scss" >
+  .container{
+    height: 300px;
+    background: red;
   }
-}
 </style>
