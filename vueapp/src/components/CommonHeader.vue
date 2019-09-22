363ae3397c005db02554b84cdbd0dcc8330b2804
AlexanderDsNie <997951737@qq.com>
2019-09-22 15:00:04 +0800

    hu

--- vueapp/src/components/CommonHeader.vue
+++ vueapp/src/components/CommonHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="header" :style="{background:$store.state.color}" >
-        {{$store.state.title}}
+        <span class="one_1">首页</span>{{$store.state.title}}
     </div>
 </template>
 
@@ -13,9 +13,17 @@
 <style lang="scss" scoped>
     .header{
         height: 50px;
+        line-height: 50px;
         background: red;
         position: fixed;
+        text-align: center;
         top:0px;
         width:100%;
+        color: white;
+    }
+    .one_1{
+        position:fixed;
+        top: 0;
+        left: 10px;
     }
 </style>
