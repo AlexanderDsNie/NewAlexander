dc9c9e8003c989c95927dadeaa07852835d97c13
AlexanderDsNie <997951737@qq.com>
2019-09-28 15:03:53 +0800

    ascd

--- vueapp/src/views/Movie.vue
+++ vueapp/src/views/Movie.vue
@@ -11,24 +11,58 @@
                 </div>
             </li>
         </ul>
+        <img class="loading" v-show="isShow" src="@/assets/images/loading1.gif" alt="">
     </div>
 </template>
+
 
 <script>
     import axios from 'axios';
     export default {
         data(){
             return {
-                movieList:[]
+                movieList:[],
+                isShow:false,
+                flag:true
             }
         },
         created(){
-            axios.get('./data/movie0.json')
-            .then((res)=>{
-                this.movieList= res.data.subjects;
-            }).catch((err)=>{
-                console.log(err);
-            })
+            this.getMovieList(this.movieList.length);
+            window.onscroll = () => {
+                let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+                let clientHeight = document.documentElement.clientHeight;
+                let scrollHeight = document.documentElement.scrollHeight;
+                console.log(scrollHeight,scrollTop ,clientHeight);
+                if( Math.abs(scrollHeight - scrollTop - clientHeight) <1 ){
+                    this.getMovieList(this.movieList.length);
+                }
+            }
+        },
+        methods:{
+            getMovieList(num){
+                this.isShow = true;
+                if(this.flag){
+                    this.flag = false;
+                     axios.get('https://bird.ioliu.cn/v1?url=https://api.douban.com/v2/movie/in_theaters?apikey=0df993c66c0c636e29ecbb5344252a4a&start='+num+'&count=10')
+                    .then((res)=>{
+                        this.movieList = [...this.movieList,...res.data.subjects];
+                        this.isShow = false;
+                        this.flag = true; 
+                    }).catch((err)=>{
+                        console.log(err);
+                    })
+                }
+               
+            }
+        }
+    }
+            // axios.get('https://bird.ioliu.cn/v1?url=https://api.douban.com/v2/movie/in_theaters?apikey=0df993c66c0c636e29ecbb5344252a4a&start=0&count=10')
+            // axios.get('./data/movie0.json')
+            // .then((res)=>{
+            //     this.movieList= res.data.subjects;
+            // }).catch((err)=>{
+            //     console.log(err);
+            // })
             // axios.get('https://bird.ioliu.cn/v1?url=https://douban.uieee.com/v2/movie/in_theaters?start=0&count=10')
             // .then((res)=>{
             //     console.log(res.data);
@@ -36,10 +70,8 @@
             // }).catch((res)=>{
             //     console.log(res);
             // })
-        }
-    }
-</script>
 
+</script>
 <style lang="scss" scoped>
 .movie-box{
     padding: 0.2rem;
@@ -52,5 +84,13 @@
         margin-bottom:0.2rem;
         border-bottom: 1px solid #000;
     }
+}
+.loading{
+    width:1.5rem;
+    // height:3rem;
+    position: fixed;
+    left:50%;
+    top:50%;
+    transform: translate(-50%,-50%);
 }
 </style>
--- vueapp/src/views/Music.vue
+++ vueapp/src/views/Music.vue
@@ -1,15 +1,40 @@
 <template>
     <div>
-        音乐
+        <ul class="music-box">
+            <li class="musicList"  v-for="(obj,index) in musicList" :key="index">
+                <div>
+                    <h2>{{obj.original_title}}</h2>
+                    <p>作者：<span v-for="(author,index) in obj.authors" :key="index">{{author.name}}</span></p>
+                    <p>上映时间：{{obj.year}}</p>
+                </div>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
+    import axios from 'axios';
     export default {
-        
+        data(){
+            return {
+                musicList:[]
+            }
+        },
+        creat(){
+            axios.get('./data/musicdata.json')
+            .then((res)=>{
+                this.musicList= res.data.subjects;
+            }).catch((err)=>{
+                console.log(err);
+            })
+        }     
     }
 </script>
 
 <style lang="scss" scoped>
-
+.music-box{
+    padding: 0.2rem;
+    .musicList{
+    }
+}
 </style>
